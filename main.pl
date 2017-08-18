609bdd187eca4df23ec8055adb42adc67a3d2bd3
Jonathon Roberts <jon@jonrob.net>
2017-08-18 18:20:57 +0100

    Cleaned code

--- main.pl
+++ main.pl
@@ -1,15 +1,15 @@
 #! /usr/bin/perl
-
 use warnings;
 use strict;
 use WWW::Mechanize;
 use HTML::TreeBuilder::XPath;
 
 sub scraper(){
-	# Get files we've already downloaded
+	# Get list of files already downloaded
 	my $dir = $_[0];
 	my $url=$_[1];
 	my @ls;
+
 	unless(-d $dir){
 		mkdir $dir;
 	}
@@ -22,80 +22,81 @@ sub scraper(){
 	chdir $dir;
 
 
+	# Initialise modules
 	my $mech=WWW::Mechanize->new();
 	$mech->get($url);
-
 	my $tree=HTML::TreeBuilder::XPath->new();
-
 	$tree->parse($mech->content);
 
+	my $file;	# Working file
+	my $skip = 0;	# To get out of nested loop
+	my $localsize;	# File size on disk
+	my $size;	# File size on server
+	my $href=5; 	# Skip headers(Name,Last Modified, Size,
+			# Description, Parent Directory
+	my $workingnode;# XPath node for pre
 	my @nodes = $tree->findnodes('/html/body');
-
-	my $file;
-	my $skip = 0;
-	my $localsize;
-	my $size;
-	my $workingnode;
 	my @hrefnodes = $tree->findnodes('/html/body/pre/a');
-	my $href=5;
+	$workingnode = $nodes[0]->findvalue( 'pre');
 
-	for my $node (@nodes) {
+	# Parse lines to get file sizes
+	while($workingnode =~ s/(.+)\s+\d\d-\w{3}-\d{4} \d\d:\d\d\s+(\S+)//){
 
-		$workingnode = $node->findvalue( 'pre');
-		# Parse line
-		for(my $i = 0;$i<1;$i++){#Skip parent directory
-			$workingnode =~ s/(.+)\s+\d\d-\w{3}-\d{4} \d\d:\d\d\s+(\S+)//;
+		#we must use href or we can't see long file names
+		if($1 =~ /Parent Directory/){next;}
+		$skip = 0;
+		$file = $hrefnodes[$href++]->findvalue('@href');
+		$size = $2;
+		$file =~ s/^\s+(.+)/$1/;
+		$file =~ s/(\S+)\s+$/$1/;
+
+		if($size =~ /-/){ #directories
+			&scraper("$dir/$file","$url/$file");
+			chdir "..";
+			next;
 		}
-		while($workingnode =~ s/(.+)\s+\d\d-\w{3}-\d{4} \d\d:\d\d\s+(\S+)//){
-
-			$skip = 0;
-			#we must use href or we can't see long file names
-			$file = $hrefnodes[$href++]->findvalue('@href');
-			$size = $2;
-			$file =~ s/^\s+(.+)/$1/;
-			$file =~ s/(\S+)\s+$/$1/;
-			#if($file=~/Name/){next;}
-			#if($file=~/Parent Directory/){next;}
-
-			if($size =~ /-/){ #directories
-				&scraper("$dir/$file","$url/$file");
-				chdir $dir;
-				$skip++;
-				next;
-			}
-			foreach(@ls){
-				#don't redownload anything we already have
-				#unless it's bigger on the server
-				if($file =~ /$_$/){
-					if($size=~s/M//){
-						$size *= 1000000;
-						$size -= 750000;
-					}
-					if($size=~s/k//){
-						$size *= 1000;
-						$size -= 750;
-					}
-					$localsize = -s $file;
-					if($size>$localsize){
-						`rm $file`;
-						`rm $file.*`;
-						#print "we already have $file,";
-						#print "locally it is $localsize bytes";
-						#print "and on the server it is";
-						#print "$size bytes\n";
-					}
-					else{
-						$skip++;
-					}
-
+		foreach(@ls){
+			# Don't redownload anything we already have
+			# unless it's bigger on the server as we may
+			# have previously downloaded an incomplete file
+			if($file =~ /$_$/){
+				if($size=~s/M//){
+					$size *= 1000000;
+					$size -= 750000;
 				}
+				if($size=~s/k//){
+					$size *= 1000;
+					$size -= 750;
+				}
+				$localsize = -s $file;
+				if($size>$localsize){
+					`rm $file`;
+					`rm $file.*`;
+					#print "we already have $file, locally";
+					#print "it is $localsize bytes and on";
+					#print "the server it is $size bytes\n";
+				}
+				else{
+					$skip++;
+				}
+
 			}
-			unless($skip){
-				'axel $url/$file';
-			}
+		}
+		unless($skip){
+			system"axel $url/$file";
 		}
 	}
 }
-my $dir = '/var/www/html/TempleOS/Videos';
-my $url = "http://templeos.org/Videos";
+
+my $dir;
+my $url;
+
+if(defined($ARGV[1]) && defined($ARGV[2])){
+	$url = $ARGV[1];
+	$dir = $ARGV[2];
+}else{
+	$dir = '/var/www/html/TempleOS/Videos';
+	$url = "http://templeos.org/Videos";
+	$dir = ".";
+}
 &scraper($dir,$url);;
